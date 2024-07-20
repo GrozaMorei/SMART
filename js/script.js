942a58cf6cd4c42036e839e0d6e2c44647522e13
GrozaMorei <81432133+GrozaMorei@users.noreply.github.com>
2024-07-20 13:13:36 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector('#burger').addEventListener('click', function () {
+	document.querySelector('#menu').classList.toggle('active');
+	document.querySelector('#burger').classList.toggle('active');
+});
